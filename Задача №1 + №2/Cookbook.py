e6e1cbf37490c1840cb985eab9657ee79d6f40e6
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2020-12-01 21:17:10 +0300

    Созданы папки для отдельных задач. Подправлена задача №1, №2. Полностью переделана задача №3

---
+++ Задача №1 + №2/Cookbook.py
@@ -0,0 +1,59 @@
+def turn_into_dict():
+    list_of_recipes = []
+    cook_book = {}
+    with open('recipes', encoding='utf-8') as recipe:
+        text = recipe.read().split('\n')
+    element = ''
+    for string in text:
+        if string != '':
+            element += string + '\n'
+            if string == text[-1]:
+                list_of_recipes.append(element)
+        else:
+            list_of_recipes.append(element)
+            element = ''
+    for recipe in list_of_recipes:
+        splited_recipe = recipe.split('\n')
+        cook_book[splited_recipe[0]] = []
+        count = int(splited_recipe[1])
+        list_of_ingredients = splited_recipe[2:count + 2]
+        for ingredient in list_of_ingredients:
+            ingredient_description = ingredient.split('|')
+            cook_book[splited_recipe[0]].append(dict(ingredient_name=ingredient_description[0].strip(),
+            quantity=int(ingredient_description[1]), measure=ingredient_description[2].strip()))
+    return cook_book
+
+
+cook_book = turn_into_dict()
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredient_name_list = []
+    shop_list = {}
+    for dish, list_of_ingredients in cook_book.items():
+        if dish in dishes:
+            for ingredient_descriptions in list_of_ingredients:
+                ingredient_value = ingredient_descriptions.get('ingredient_name')
+                quantity_value = ingredient_descriptions.get('quantity')
+                ingredient_name_list.append(ingredient_value)
+                count = ingredient_name_list.count(ingredient_value)
+                del ingredient_descriptions['ingredient_name']
+                if count == 1:
+                    shop_list[ingredient_value] = ingredient_descriptions
+                else:
+                    shop_list[ingredient_value]['quantity'] += quantity_value
+    for key, value in shop_list.items():
+        value['quantity'] = value['quantity'] * person_count
+    return shop_list
+
+
+print(get_shop_list_by_dishes(['Омлет','Фахитос'], 2))
+
+
+
+
+
+
+
+
+
---
+++ Задача №3/ConnectingFiles.py
@@ -0,0 +1,31 @@
+import os
+
+
+def sort_and_connect_files(file_folder):
+    files_list = os.scandir(file_folder)
+    full_text_list = []
+    full_text = ''
+    for file in files_list:
+        with open(file, encoding='utf-8') as f:
+            splited_text = f.readlines()
+            text_len = str(len(splited_text)) + '\n'
+            file_name = file.name + '\n'
+            splited_text.insert(0, text_len)
+            splited_text.insert(0, file_name)
+            full_text_list.append(splited_text)
+    sorted_text_list = sorted(full_text_list, key=len)
+    for text in sorted_text_list:
+        full_text += ''.join(text) + '\n'
+    return full_text
+
+
+enhanced_text = sort_and_connect_files('Задача №3/Text files')
+
+
+def write_a_new_file(text):
+    with open('New_File', 'w', encoding='utf-8') as final_file:
+        final_file.write(text)
+    return ''
+
+
+print(write_a_new_file(enhanced_text))
